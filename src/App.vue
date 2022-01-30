6b7f57c9ff0f3f52006f3b8b78c8f1d8f6f10218
DavSanosyan0145 <dav.sanosyan.dev@gmail.com>
2022-01-30 22:12:43 +0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,79 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Header />
+  <div> Counter: {{ counter }}</div>
+  <span v-bind:title="msg">hover</span>
+  <button @click="click">click</button>
+  <!--  -->
+  <p>{{ inputMsg }}</p>
+  <input v-model="inputMsg" />
+  <!--  -->
+  <div>
+    <div v-bind:key="i.id" v-for="i in data">
+      {{ i }}
+      <span v-if="i.name">aaaaa</span>
+      <span v-else-if="i.nick">{{ i.nick }}</span>
+      <span v-else>bbbbb</span>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./layouts/Header/Header.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Header,
+  },
+  
+  
+  methods: {
+    click () {
+      this.counter++
+    }
+  },
+
+  data () {
+  return {
+    counter: 0,
+    publicPath: 'hello',
+    inputMsg: 'hello',
+    data : [
+      {id: 1, text: 'text1', name: true},
+      {id: 2, text: 'text2', name: false},
+      {id: 3, text: 'text3', name: true},
+      {id: 4, text: 'text4', name: false, nick: 'kampot'},
+      ]
   }
-}
+},
+
+  renderTriggered({ key, target, type }) {
+    console.log({ key, target, type })
+  },
+  renderTracked({ key, target, type }) {
+    console.log({ key, target, type })
+  },
+  updated () {
+    this.$nextTick(function () {
+          return 'a'
+    // Code that will run only after the
+    // entire view has been re-rendered
+  })
+  },
+beforeCreate () {
+  console.log(1)
+},
+created () {
+  console.log(2)
+},
+mounted () {
+  console.log(3)
+    //   setInterval(() => {
+    //   this.counter++
+    // }, 1000)
+},
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
